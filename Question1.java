c8921412681e39f36b6c235c4659b6b2e8f80ea9
roofclimb <s10243726@connect.np.edu.sg>
2023-04-17 17:17:28 +0800

    qn1

--- Question1.java
+++ Question1.java
@@ -2,7 +2,7 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World!")
+    System.out.println("Hello World!");
     /**
      * Print "Hello World!"
      * 
