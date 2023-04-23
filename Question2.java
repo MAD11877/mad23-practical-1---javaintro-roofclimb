eea8b74bf08bf0efb3b1b5396585759156d2683a
roofclimb <s10243726@connect.np.edu.sg>
2023-04-23 21:06:35 +0800

    qn2

--- Question2.java
+++ Question2.java
@@ -10,13 +10,14 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
     
-    System.out.print("Height: ");
+    //System.out.print("Height: ");
 
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
-    System.out.print("Weight: ");
+    //System.out.print("Weight: ");
     double weight = in.nextDouble();
     double bmi=weight/(height*height);
-    System.out.println("BMI: "+bmi);
+    //System.out.println("BMI: "+bmi);
+    System.out.println(bmi);
     }
 }
